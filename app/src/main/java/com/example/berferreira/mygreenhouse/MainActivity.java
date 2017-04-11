71b9f33af753d5dd8d7b24f0fbddc9b5c02fb122
Canh0to <Canh0to@hotmail.com>
2017-04-11 17:16:47 +0100

    Teste 2

--- app/src/main/java/com/example/berferreira/mygreenhouse/MainActivity.java
+++ app/src/main/java/com/example/berferreira/mygreenhouse/MainActivity.java
@@ -30,9 +30,6 @@ public class MainActivity extends AppCompatActivity {
                 temperatureText.setText("");
                 humidityText.setText("");
 
-                //Benfica é............
-                //teste
-                //teste
             }
         });
     }
